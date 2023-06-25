6a738b294398717696fbb27127060317d926f6cb
Ирина Капкова <kapiral@rambler.ru>
2023-06-25 22:22:00 +0400

    Initial commit

---
+++ src/main/java/com/example/additionalmodule/AdditionalModuleApplication.java
@@ -0,0 +1,13 @@
+package com.example.additionalmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdditionalModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdditionalModuleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/additionalmodule/AdditionalModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.additionalmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdditionalModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
